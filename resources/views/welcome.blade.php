add12244776b8871f93460b388d099db1d2aece9
Lucille <82943831+luciadobre@users.noreply.github.com>
2024-01-19 16:17:46 +0200

    filtru

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,15 +4,16 @@
     <div class="container">
         <h1>Articole</h1>
         @foreach ($articles as $article)
-            <div class="card mb-3" onclick="handleArticleClick('{{ $article->id }}')">
-                <div class="card-header">{{ $article->title }}</div>
-                <div class="card-body">
-                    <p>{{ Str::limit($article->content, 100) }}</p>
-                    <p>Categorie: <strong> {{ $article->category }} </strong> </p>
+            @if($article->status != 'respins' && $article->status != 'in asteptare')
+                <div class="card mb-3" onclick="handleArticleClick('{{ $article->id }}')">
+                    <div class="card-header">{{ $article->title }}</div>
+                    <div class="card-body">
+                        <p>{{ Str::limit($article->content, 100) }}</p>
+                        <p>Categorie: <strong> {{ $article->category }} </strong> </p>
+                    </div>
                 </div>
-            </div>
+            @endif
         @endforeach
-    </div>
 
     <script>
         function handleArticleClick(articleId) {
